ae0e92964213d54026f3850716affd5bd08535f4
Alan Sampson <alansamps@gmail.com>
2021-01-06 19:30:49 +0000

    Provide for alternate data inputs.

--- main.cpp
+++ main.cpp
@@ -5,22 +5,53 @@
 #include <algorithm>
 #include <numeric>
 #include <functional>
+#include <utility>
+#include <random>
 
 size_t constexpr max_elmts(10);
 
+enum Style {
+  PROMPT, RANDOM, FIXED,
+};
+
 inline
-std::vector<int> & collect(std::vector<int> &);
+std::vector<int> & collect(std::vector<int> &, Style style = PROMPT);
 inline
 void show(std::vector<int> const &);
-
 void do_it_with_an_array(size_t len, int * ap);
 
 /*
  *  MARK: main()
  */
 int main(int argc, char const * argv[]) {
+
+  Style style;
+  int choice(0);
+  while (choice == 0) {
+    std::cout << "Menu:\n"
+              << "  1: Prompt for input\n"
+              << "  2: Use fixed input\n"
+              << "  3: Use random input\n";
+    std::cout.flush();
+    std::cin >> choice;
+    switch (choice) {
+    case 1:
+      style = PROMPT;
+      break;
+    case 2:
+      style = FIXED;
+      break;
+    case 3:
+      style = RANDOM;
+      break;
+    default:
+      choice = 0;
+      break;
+    }
+  }
+
   std::vector<int> av(max_elmts);
-  av = collect(av);
+  av = collect(av, style);
   show(av);
 
   auto sum = std::accumulate(av.cbegin(), av.cend(), 0);
@@ -71,7 +102,6 @@ void do_it_with_an_array(size_t len, int * ap) {
             << "elements .GT. average: " << nct << '\n'
             << std::endl;
 
-
   std::vector<int> rv(nct);
   std::copy_if(std::cbegin(aa),std::cend(aa), rv.begin(), ip);
   std::sort(rv.begin(), rv.end(), std::greater_equal<>());
@@ -86,19 +116,54 @@ void do_it_with_an_array(size_t len, int * ap) {
  *  MARK: collect()
  */
 inline
-std::vector<int> & collect(std::vector<int> & av) {
+std::vector<int> & collect(std::vector<int> & av, Style style) {
   size_t av_s = av.size();
-  int val(0);
-  for (size_t ct(0); ct < av_s; ++ct) {
-    val = 0;
-    while (val % 2 == 0) {
-      std::cout << "element " << std::setw(2) << ct + 1 << ": ";
-      std::cout.flush();
-      std::cin >> val;
-      av[ct] = val;
+
+  switch (style) {
+  case RANDOM:
+    {
+      std::random_device rd; //used to obtain a seed for the prng
+      std::mt19937 prng(rd()); //mersenne_twister prng seeded w. rd()
+      std::uniform_int_distribution<> distrib(100, 9'999);
+      std::vector<int> nr(av.size());
+      std::iota(nr.begin(), nr.end(), 0);
+      for (auto ix : nr) {
+        int rn(0);
+        do {
+          rn = distrib(prng);
+        } while (rn % 2 == 0);
+        av[ix] = rn;
+      }
     }
+    break;
+
+  case FIXED:
+    {
+      std::vector<int> fx {
+        11, 13, 15, 17, 19, 21, 23, 25, 27, 29,
+      };
+      av.clear();
+      std::move(fx.begin(), fx.end(), std::back_inserter(av));
+    }
+    break;
+
+  case PROMPT:
+  default:
+    {
+      int val(0);
+      for (size_t ct(0); ct < av_s; ++ct) {
+        val = 0;
+        while (val % 2 == 0) {
+          std::cout << "element " << std::setw(2) << ct + 1 << ": ";
+          std::cout.flush();
+          std::cin >> val;
+          av[ct] = val;
+        }
+      }
+      std::cout << std::endl;
+    }
+    break;
   }
-  std::cout << std::endl;
 
   return av;
 }
